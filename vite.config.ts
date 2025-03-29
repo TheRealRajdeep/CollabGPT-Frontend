5c4354c67874dc6a88e5e534bea80d11ceefc678
therealrajdeep <rajdeepjadeja2806@gmail.com>
2025-03-29 15:22:40 +0530

    add railway host to vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    allowedHosts: ['collabgpt-frontend-production.up.railway.app']
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
